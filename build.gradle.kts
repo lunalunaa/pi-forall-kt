8f58562e5685082b1f2c76e5abbabf49690a47df
Luna Xin <weitaoxin@gmail.com>
2021-07-10 21:45:02 +0800

    bump kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.5.20"
     application
 }
 
-group = "me.mono"
+group = "me.luna"
 version = "1.0-SNAPSHOT"
 
 repositories {
