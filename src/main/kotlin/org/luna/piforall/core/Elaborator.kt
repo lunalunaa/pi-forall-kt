611604453eb0928fe692ac57202a00656331c251
Luna Xin <weitaoxin@gmail.com>
2021-07-14 10:53:23 +0800

    refactor elaborator

--- src/main/kotlin/org/luna/piforall/core/Elaborator.kt
+++ src/main/kotlin/org/luna/piforall/core/Elaborator.kt
@@ -27,7 +27,7 @@ data class Context(
     }
 
     companion object {
-        fun emptyCxt(): Context {
+        fun emptyContext(): Context {
             return Context(emptyList(), emptyList(), 0)
         }
     }
@@ -35,42 +35,41 @@ data class Context(
 
 // TODO: refactor code so that context is a member of the elaborator
 
-class Elaborator(private val debugMode: Boolean) {
+class Elaborator(
+    private val context: Context = Context.emptyContext(),
+    private val debugMode: Boolean = false
+) {
 
     @Throws(TypeCheckError::class)
-    fun checkTy(ctx: Context, ct: CTerm, expected: VType): Term {
-        if (debugMode) debugChecker(ctx, ct, expected)
+    fun checkTy(ct: CTerm, expected: VType): Term {
+        if (debugMode) debugChecker(context, ct, expected)
 
         return if (ct is CTerm.CLam && expected is Value.VPi) {
-            val tm =
-                checkTy(
-                    ctx.bind(ct.binder, expected.dom),
-                    ct.body,
-                    expected.codom.applyTo(lazy { Value.VVar(ctx.lvl) })
-                )
+            val extendedElab = Elaborator(context.bind(ct.binder, expected.dom), debugMode)
+            val tm = extendedElab.checkTy(ct.body, expected.codom.applyTo(lazy { Value.VVar(context.lvl) }))
             Term.Lam(ct.binder, tm)
         } else {
-            val (tm, inferred) = inferTy(ctx, ct)
-            if (checkConv(ctx.lvl, expected, inferred)) tm else throw TypeMismatch(expected, inferred, ct)
+            val (tm, inferred) = inferTy(ct)
+            if (checkConv(context.lvl, expected, inferred)) tm else throw TypeMismatch(expected, inferred, ct)
         }
     }
 
     @Throws(TypeCheckError::class)
-    fun inferTy(ctx: Context, ct: CTerm): Pair<Term, VType> {
+    fun inferTy(ct: CTerm): Pair<Term, VType> {
 
-        if (debugMode) debugInferer(ctx, ct)
+        if (debugMode) debugInferer(context, ct)
 
         return when (ct) {
 
-            is CTerm.CVar -> ctx.types.run {
+            is CTerm.CVar -> context.types.run {
                 val idx = indexOfFirst { (name, _) -> name == ct.name }
                 return if (idx != -1) Pair(Term.Var(idx), get(idx).second) else throw VarOutOfScope(ct)
             }
             is CTerm.CApp -> {
-                val (tm1, inferred) = inferTy(ctx, ct.tm1)
+                val (tm1, inferred) = inferTy(ct.tm1)
                 if (inferred is Value.VPi) {
-                    val tm2 = checkTy(ctx, ct.tm2, inferred.dom.value)
-                    Pair(Term.App(tm1, tm2), inferred.codom.applyTo(lazy { Normalizer(ctx.env).eval(tm2) }))
+                    val tm2 = checkTy(ct.tm2, inferred.dom.value)
+                    Pair(Term.App(tm1, tm2), inferred.codom.applyTo(lazy { Normalizer(context.env).eval(tm2) }))
                 } else {
                     throw ExpectedFunType(inferred)
                 }
@@ -79,26 +78,17 @@ class Elaborator(private val debugMode: Boolean) {
 
             // I think Pi can be either synthesized or checked
             is CTerm.CPi -> {
-                val dom = checkTy(ctx, ct.dom, Value.VUniv)
-                val codom = checkTy(ctx.bind(ct.binder, lazy { Normalizer(ctx.env).eval(dom) }), ct.codom, Value.VUniv)
+                val dom = checkTy(ct.dom, Value.VUniv)
+
+                // defines a new elaborator which binds the binder with the codomain type
+                val extendedElab =
+                    Elaborator(context.bind(ct.binder, lazy { Normalizer(context.env).eval(dom) }), debugMode)
+                val codom = extendedElab.checkTy(ct.codom, Value.VUniv)
                 Pair(Term.Pi(ct.binder, dom, codom), Value.VUniv)
             }
             is CTerm.CUniv -> Pair(Term.Univ, Value.VUniv)
         }
     }
-
-    /**
-     * Check type without context
-     */
-    @Throws(TypeCheckError::class)
-    fun checkTy(ct: CTerm, expected: VType): Term = checkTy(Context.emptyCxt(), ct, expected)
-
-    /**
-     * Infer type without context
-     */
-    @Throws(TypeCheckError::class)
-    fun inferTy(ct: CTerm): Pair<Term, VType> = inferTy(Context.emptyCxt(), ct)
-
 }
 
 fun main() {
